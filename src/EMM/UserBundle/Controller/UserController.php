b11abd74675d17e95fc84950cade4c87ade70fa1
Alfonso Cabezas Fernández <alfonso.cabezas@eurotransportcar.com>
2024-03-15 11:09:53 +0100

    Usuario

--- src/EMM/UserBundle/Controller/UserController.php
+++ src/EMM/UserBundle/Controller/UserController.php
@@ -9,6 +9,30 @@ class UserController extends Controller
 {
     public function indexAction()
     {
-        return new Response('Bienvenido a mi módulo de usuarios');
+
+        //return new Response('Bienvenido a mi módulo de usuarios');
+
+        $em = $this->getDoctrine()->getManager();
+
+        $users = $em->getRepository('EMMUserBundle:User')->findAll();
+
+        $res = 'Lista de usuarios: <br />';
+
+        foreach ($users as $user) {
+            $res .= 'Usuario: ' . $user->getUsername() . ' - Email: ' . $user->getEmail() . '<br />';
+        }
+
+        return new Response($res);
+    }
+
+    public function viewAction($id)
+    {
+        $repository = $this->getDoctrine()->getRepository('EMMUserBundle:User');
+
+        $user = $repository->find($id);
+
+        // $user = $repository->findOneByUsername($nombre);
+
+        return new Response('Usuario: ' . $user->getUsername() . ' con email: ' . $user->getEmail());
     }
 }
